5bde99010925e9e017792238365d4dab636377bf
Panagiotis Dorgiakis <p.dorgiak@gmail.com>
2017-05-18 21:22:10 +0300

    Generating the population with which we will start finding the fitness for the progress in our program. Each individual in the population has it's DNA and every letter represents the genes

--- DNA.py
+++ DNA.py
@@ -4,12 +4,29 @@ import random
 
 class DNA:
 
-    def __init__(self, DNA_LENGTH):
+    def __init__(self,  DNA_LENGTH, target, genes=None):
+        if genes == None:
         self.generate_genes(DNA_LENGTH)
+        elif genes != None:
+            self.genes = genes
+        self.target = target
+        self.DNA_LENGTH = DNA_LENGTH
+
+
 
     def generate_genes(self, DNA_LENGTH):
-        chars_pool = string.printable
         self.genes = []
         for i in range(0, DNA_LENGTH):
-            gene = chars_pool[random.random(0, len(chars_pool))]
+            gene = self.getChar()
             self.genes.append(gene)
+
+    def generate_genes(self, DNA_LENGTH):
+        self.genes = []
+        for i in range(0, DNA_LENGTH):
+            gene = self.getChar()
+            self.genes.append(gene)
+
+    def getChar(self):
+        chars_pool = string.printable
+        char = chars_pool[random.randint(0, len(chars_pool) - 1)]
+        return char
--- Population.py
+++ Population.py
@@ -4,6 +4,10 @@ class Population:
     def __init__(self, TotalPopulation, target):
         self.population = []
         self.target = target
-        for i in range(0,TotalPopulation):
-            self.population.append(DNA(len(target)))
+
+        self.populate()
+    def populate(self):
+        self.population = []
+        for i in range(0, self.TotalPopulation):
+            self.population.append(DNA(len(self.target), self.target))
     
