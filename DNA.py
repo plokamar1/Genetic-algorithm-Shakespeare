0690ef780a63d8eb027e3d5e5f070cba8706c332
Panagiotis Dorgiakis <p.dorgiak@gmail.com>
2017-05-18 21:38:18 +0300

    Generating the fitness for each individual in the population. The lists 'population' and 'fitness' are parallel lists. Meaning that the first individual's fitness is the first fitness in the 'fitness' list.

--- DNA.py
+++ DNA.py
@@ -20,13 +20,18 @@ class DNA:
             gene = self.getChar()
             self.genes.append(gene)
 
-    def generate_genes(self, DNA_LENGTH):
-        self.genes = []
-        for i in range(0, DNA_LENGTH):
-            gene = self.getChar()
-            self.genes.append(gene)
-
     def getChar(self):
         chars_pool = string.printable
         char = chars_pool[random.randint(0, len(chars_pool) - 1)]
         return char
+
+    def get_fitness(self):
+        i = 0
+        fit_points = 0
+        for gene in self.genes:
+            if(gene == self.target[i]):
+                fit_points += 1
+            i += 1
+        self.fit_points = int((fit_points/self.DNA_LENGTH )* 100)
+        return(self.fit_points)
+
--- Population.py
+++ Population.py
@@ -1,13 +1,22 @@
 from DNA import *
 
 class Population:
-    def __init__(self, TotalPopulation, target):
+    def __init__(self, TotalPopulation, target, mutationRate):
+        self.fitness = []
         self.population = []
         self.target = target
+        self.TotalPopulation = TotalPopulation
 
         self.populate()
+        self.get_fitness()
+
     def populate(self):
         self.population = []
         for i in range(0, self.TotalPopulation):
             self.population.append(DNA(len(self.target), self.target))
+
+    def get_fitness(self):
+        for individual in self.population:
+            self.fitness.append(individual.get_fitness())
+        
     
